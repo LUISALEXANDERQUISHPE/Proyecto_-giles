137adad794212e84cb2d85c6f700b02e42d2b542
Christian Hurtado <tcriss002@gmail.com>
2024-06-07 23:44:41 -0500

    Se añadio la ventana Crear Informe

---
+++ src/Components/CreateInforme/CreateInforme.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './CreateInforme.css'; // Asegúrate de que esta línea no esté comentada
+import { FaUser, FaSignOutAlt, FaQuestionCircle } from 'react-icons/fa';
+import { MdSchool } from 'react-icons/md';
+import { IoMdPeople } from 'react-icons/io';
+import { RiVipCrownFill } from "react-icons/ri";
+
+const CreateInform = ({ handleLogout }) => {
+  const location = useLocation();
+
+  return (
+    <div className="sidebar">
+      <div className="header">
+        <RiVipCrownFill className='icon2'/>
+        <span>FISEI</span>
+      </div>
+      <div className="nav-menu">
+        <div className={`menu-item ${location.pathname === '/profile' ? 'active' : ''}`}>
+          <Link to="/profile">
+            <FaUser className='icon1'/>
+            <span>Mi perfil</span>
+          </Link>
+        </div>
+        <div className={`menu-item ${location.pathname === '/register-student' ? 'active' : ''}`}>
+          <Link to="/register-student">
+            <MdSchool className='icon1'/>
+            <span>Registro de <br />estudiante</span>
+          </Link>
+        </div>
+        <div className={`menu-item ${location.pathname === '/Student' ? 'active' : ''}`}>
+          <Link to="/Student">
+            <IoMdPeople className='icon1'/>
+            <span>Estudiantes</span>
+          </Link>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="menu-item">
+          <Link to="/" onClick={handleLogout}>
+            <FaSignOutAlt />
+            <span>Cerrar sesión</span>
+          </Link>
+        </div>
+        <div className="menu-item">
+          <Link to="/help">
+            <FaQuestionCircle />
+            <span>Ayuda y soporte</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateInform;
